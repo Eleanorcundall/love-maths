a409a22334344a2dd283310f944ed206a810ce8a
Eleanor Cundall <eleanorcundall@hotmail.com>
2023-07-25 20:12:46 +0100

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,40 @@
- function runGame() {
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("hello");
+  let buttons = document.getElementsByTagName("button");
 
- }
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+/** 
+ * The main game "loop", called when the script is first loaded 
+ * and after the users answer has been processed 
+*/
+function runGame() {
 
- function checkAnswer(){
+    // Creates 2 random numbers 
 
- }
+    let num1 = Math.floor(Math.random() * 25) + 1
+    let num2 = Math.floor(Math.random() * 25) + 1
+}
 
- function calculateCorrectAnswer(){
+function checkAnswer() {}
 
- }
+function calculateCorrectAnswer() {}
 
- function incrementScore(){
+function incrementScore() {}
 
- }
+function incrementWrongAnswer() {}
 
- function incrementWrongAnswer(){
+function displayAdditionQuestion() {}
 
- }
+function displaySubtractQuestion() {}
 
- function displayAdditionQuestion(){
-
- }
-
- function displaySubtractQuestion(){
-
-
- }
-
- function displayMultiplyQuestion(){
-
- }
+function displayMultiplyQuestion() {}
